40c6081aeddda7f613a9d0dc24440e8e23b76dcc
Newton <altontonnalumasa@gmail.com>
2022-12-22 22:27:09 +0300

    add cards and form selection

---
+++ team-allocation/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header";
+import Employees from "./Employees";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Employees />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
