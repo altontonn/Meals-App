274e4fe8f53cc04ec2350571ccaeb0f8104f7e39
Newton Alumasa <altontonnalumasa@gmail.com>
2022-12-30 14:08:04 +0300

    Delete team-member-allocation directory

--- team-member-allocation/src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <h1>Footer</h1>
-    </footer>
-  );
-};
-export default Footer;
--- team-member-allocation/src/Header.jsx
+++
@@ -1,8 +0,0 @@
-const Header = () => {
-  return (
-    <header>
-      <h1 className="mt-4 p5 bg-primary text-white rounded">Header</h1>
-    </header>
-  );
-};
-export default Header;
