138c4fe95a14e85a4ab1adc330a4dfc234b6b886
Newton <altontonnalumasa@gmail.com>
2023-01-04 18:08:06 +0300

    fix netlify errors

--- src/Components/Favorites.jsx
+++ src/Components/Favorites.jsx
@@ -1,19 +1,24 @@
-import { useGlobalContext } from "../Context";
+import { useGlobalContext } from '../Context';
+
 const Favorites = () => {
-  const {favorites, selectMeal, removeFromFavorites} = useGlobalContext();
-  return <section className="favorites">
+  const { favorites, selectMeal, removeFromFavorites } = useGlobalContext();
+  return (
+    <section className="favorites">
       <div className="favorites-content">
         <h5>Favorites</h5>
         <div className="favorites-container">
           {favorites.map((item) => {
-            const {idMeal, strMealThumb: image} = item;
-            return <div key={idMeal} className="favorite-item">
-              <img src={image} className="favorites-img img" alt="favorite" onClick={() => selectMeal(idMeal, true)}/>
-              <button className="remove-btn" onClick={() => removeFromFavorites(idMeal)}>remove</button>
-            </div>
+            const { idMeal, strMealThumb: image } = item;
+            return (
+              <div key={idMeal} className="favorite-item">
+                <img src={image} className="favorites-img img" alt="favorite" onClick={() => selectMeal(idMeal, true)}/>
+                <button className="remove-btn" onClick={() => removeFromFavorites(idMeal)}>remove</button>
+              </div>
+              )
           })}
         </div>
       </div>
-    </section>;
+    </section>
+  )
 };
 export default Favorites;
